227b0d909e5eb4a682907326362812add582153a
gauravsutaria0010-hue <gauravsutaria0010@gmail.com>
2026-08-28 17:43:59 +0530

    Mainpagr+CS

--- RAJANI_ERP/RAJANI_ERP/RAJANI_ERP/MainPage.cs
+++ RAJANI_ERP/RAJANI_ERP/RAJANI_ERP/MainPage.cs
@@ -16,5 +16,110 @@ namespace RAJANI_ERP
         {
             InitializeComponent();
         }
+
+        private void bunifuPanel3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuLabel3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuPictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuIconButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuIconButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuButtonCompany_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuButton21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuButton22_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuButton23_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuButton26_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuButton24_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuButton25_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuPanel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuButtonSettings_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuButtonProducts_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuButtonDashboard_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuPanel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuLabel3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuLabel4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
