9e92bfdb07d4b0da35d817e686aac8d2759687fc
gauravsutaria0010-hue <gauravsutaria0010@gmail.com>
2026-09-21 22:59:18 +0530

    21-09-2026

---
+++ RAJ/Rajani_ERP/Rajani_ERP/Cancel Cash Sale.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rajani_ERP
+{
+    public partial class Cancel_Cash_Sale : Form
+    {
+        public Cancel_Cash_Sale()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ClosecancelCSFlatButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
---
+++ RAJ/Rajani_ERP/Rajani_ERP/Create Cash Sale.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rajani_ERP
+{
+    public partial class CreateCashSale : Form
+    {
+        public CreateCashSale()
+        {
+            InitializeComponent();
+        }
+
+        private void BunifuFlatButton1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BunifuButton2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PopUpForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
